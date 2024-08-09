db1c82c11cb69b883c873925226e081ec1a46517
Anthony Thanpoovong <thanpoovong84@gmail.com>
2024-08-09 14:14:31 -0400

    weather conditions

--- umbrella.py
+++ umbrella.py
@@ -7,3 +7,16 @@ from bs4 import BeautifulSoup
 city = "Hyderabad"
 url = "https://www.google.com/search?q=" + "weather" + city 
 html = requests.get(url).content 
+
+soup = BeautifulSoup(html, 
+                     'html.parser') 
+temperature = soup.find( 
+  'div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text 
+time_sky = soup.find( 
+  'div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text 
+  
+# formatting data 
+sky = time_sky.split('\n')[1] 
+
+if sky == "Rainy" or sky == "Rain And Snow" or sky == "Showers" or sky == "Haze" or sky == "Cloudy": 
+    smtp_object = smtplib.SMTP('smtp.gmail.com', 587)
